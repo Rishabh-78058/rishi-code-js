8083d99fc89a3872cca10293cc0eac143589ae5a
Rishabh Chaurasiya <rishabhchausiya78058@gmail.com>
2025-11-25 19:18:58 +0000

    conversion of data type

---
+++ rishi/text3.js
@@ -0,0 +1,33 @@
+//**********Conversion of DATA TYPE************* 
+
+// console.log(2>1)
+// console.log( 2 >=1)
+// console.log( 2 < 2)
+// console.log( 2 ==1)
+// console.log( 2 !=1)
+
+
+
+// console.log("2">1)
+// console.log ("02"> 1)
+
+
+console.log( null > 0)
+console.log( null == 0)
+console.log( null >= 0)
+
+
+
+console.log( undefined == 0)
+console.log( undefined > 0)
+console.log( undefined < 0)
+
+
+// *******In this type of conversion every time create a confusion*****
+
+
+
+// ===
+
+
+console.log("2" === 2)
