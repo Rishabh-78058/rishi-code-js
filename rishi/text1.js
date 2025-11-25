52463ee15242c9aec95f326daef012204bb430ef
Rishabh Chaurasiya <rishabhchausiya78058@gmail.com>
2025-11-25 10:44:09 +0000

    datatype done

---
+++ rishi/text1.js
@@ -0,0 +1,42 @@
+ //IN this lector we learn DATA TYPE IN JS 
+
+
+
+ "use strict"   // treat all jas code as never  version 
+
+
+    // alert (3+3) //we are using nodejs not browser 
+
+
+ console.log (3
+    +3)    
+    
+    //code readablity should be high 
+
+
+    console.log ("rishi")
+
+    let  NAME = "Rishi"
+    let age = 22    
+    let isloggedIn = false
+
+
+    //number => 2 to power 53
+    // bigint
+    //string => "" , '' , ``
+    //boolean => true/false
+    //null => standalone value
+    //undefined => variable not assigned value
+    //symbol => unique
+
+    // Objects => key value pairs
+
+
+    console.log(typeof null) //object 
+    console.log(typeof undefined) //undefined
+    console.log(typeof isloggedIn) // booliean 
+     
+    console.log(typeof age)// number
+
+    console.log(typeof NAME) //string
+    
