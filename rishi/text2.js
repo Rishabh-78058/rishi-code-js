92d6720cd405ce857a7cca00e9d5faf886df6d58
Rishabh Chaurasiya <rishabhchausiya78058@gmail.com>
2025-11-25 11:07:04 +0000

    conversion in js

---
+++ rishi/text2.js
@@ -0,0 +1,26 @@
+let score ="292"
+ 
+//console.log (typeof score) //string
+//console.log (typeof Number(score)) //number
+let valueInNumber = Number(score)
+//console.log (typeof valueInNumber) //number
+//console.log (valueInNumber ) 
+// NaN => Not a Number
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1 , false => 0
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log (booleanIsLoggedIn)
+
+
+//1 => true; 0 => false 
+//"" => false 
+//"rishi "=> true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log (stringNumber);
+console.log (typeof stringNumber);
