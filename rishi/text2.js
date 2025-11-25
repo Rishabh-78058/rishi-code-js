6823cd66760e64a091ec94f181a3e6779aa3a2b9
Rishabh Chaurasiya <rishabhchausiya78058@gmail.com>
2025-11-25 19:07:39 +0000

    conversion done

--- rishi/text2.js
+++ rishi/text2.js
@@ -24,5 +24,58 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log (stringNumber);
-console.log (typeof stringNumber);
+//console.log (stringNumber);
+//console.log (typeof stringNumber);
+
+
+//************************************************* Operations ******************************** 
+
+// *******why string to number conversion ************
+
+let value = 3
+let negValue = -value
+//console.log (negValue) 
+
+
+//console.log(2+2)
+//console.log (2-2)
+//console.log (2*2)
+//console.log (2**3)
+//console.log(2/2)
+//console.log (5%2)
+
+
+let str1 = "rishi"
+let str2 = " chaurasiya"
+
+let str3 = str1 + str2
+console.log(str3)
+
+
+// console.log ("2" + 2)
+// console.log ( 1 + "2")
+// console.log(1 + 2+ "3")
+// console.log ("3" + 1 + 2)
+// console.log((3 + 4 )* 5 % 6)
+
+
+
+// console.log(+true)
+// console.log(+"")
+
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100
+
+gamecounter = gamecounter + 1
+gamecounter ++
+++gamecounter
+console.log(gamecounter)
+
+
+
+
+
